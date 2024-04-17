6e6394a1c87f372704192b2542425616e37bda2b
Christian Schou <christian.schou@hotmail.com>
2024-04-17 15:53:43 +0200

    ✨ Create Identity Services

---
+++ src/AdminLTE.TWC.Infrastructure/Data/Identity/ApplicationUser.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace AdminLTE.TWC.Infrastructure.Data.Identity;
+
+/// <summary>
+///     An extension of <see cref="IdentityUser"/> to add more properties
+///     for a single user in the application.
+/// </summary>
+public class ApplicationUser : IdentityUser
+{
+}
---
+++ src/AdminLTE.TWC.Infrastructure/Data/Identity/IdentityResultExtension.cs
@@ -0,0 +1,22 @@
+using AdminLTE.TWC.Core.Application.Common.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace AdminLTE.TWC.Infrastructure.Data.Identity;
+
+/// <summary>
+///     Provides extension methods for <see cref="IdentityResult"/>.
+/// </summary>
+public static class IdentityResultExtension
+{
+    /// <summary>
+    ///     Converts an <see cref="IdentityResult"/> to a <see cref="Result"/>.
+    /// </summary>
+    /// <param name="result">The <see cref="IdentityResult"/> to convert.</param>
+    /// <returns>A <see cref="Result"/> that represents the same operation as the <see cref="IdentityResult"/>.</returns>
+    public static Result ToApplicationResult(this IdentityResult result)
+    {
+        return result.Succeeded
+            ? Result.Success()
+            : Result.Failure(result.Errors.Select(error => error.Description));
+    }
+}
