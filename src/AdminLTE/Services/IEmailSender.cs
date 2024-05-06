ca9f2884cafc988ba48c68db7280a7ef07db790c
Christian Schou <christian.schou@hotmail.com>
2024-05-06 14:41:03 +0200

    💡 Add docs on the services currently implemented

--- src/AdminLTE/Services/IEmailSender.cs
+++ src/AdminLTE/Services/IEmailSender.cs
@@ -1,6 +1,16 @@
 ﻿namespace AdminLTE.Services;
 
+/// <summary>
+///     A service for sending emails. Other Email services in the application, should inherit from this interface.
+/// </summary>
 public interface IEmailSender
 {
-    Task SendEmailAsync(string email, string subject, string message);
+    /// <summary>
+    ///     Send an email using the selected provider.
+    /// </summary>
+    /// <param name="address">The email address.</param>
+    /// <param name="subject">The email subject.</param>
+    /// <param name="message">The email message/body/content.</param>
+    /// <returns></returns>
+    Task SendEmailAsync(string address, string subject, string message);
 }
